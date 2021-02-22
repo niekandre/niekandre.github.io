a3d9e3bd0f61df8287f84a1cace47205c8fc876e
niekandre <niek@niekandre.nl>
2021-02-22 10:56:00 +0100

    Code edited

--- tarotcards/index.php
+++ tarotcards/index.php
@@ -8,8 +8,7 @@
 
     <div class="card"></div>
 
-    <script src="gsap.js"></script>  
-    <!-- <script type="module" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.0/gsap.min.js"></script> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.0/gsap.min.js"></script>
     <script src="script.js"></script>
 </body>
 </html>
--- tarotcards/script.js
+++ tarotcards/script.js
@@ -1,3 +1,1 @@
-import gsap from "gsap";
-
 gsap.to(".card", {rotation: 27, x: 100, duration: 1});
